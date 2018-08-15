c01be1df657a2e92abbc2f9c83e02973471aa260
Branson Camp <bcamp0@icloud.com>
2018-08-15 14:37:01 -0600

    Added optional FPS counter

--- src/gui-beta.py
+++ src/gui-beta.py
@@ -21,6 +21,7 @@ data = []
 xAxis = []
 
 pid_active = True
+show_fps = False
 
 def toggle_pid():
     global pid_active
@@ -47,7 +48,7 @@ root.title("Amorphics")
 
 backsplash_img = ImageTk.PhotoImage(Image.open('src/bigblob.png'))
 
-backsplash_canvas = Canvas(root, width=1920, height=1080)
+backsplash_canvas = Canvas(root, width= 1920, height=1080)
 
 backsplash_canvas.create_image(200, 100, anchor=NW, image=backsplash_img) 
 
@@ -68,16 +69,28 @@ diameter_label = Label(root, text="Diameter: \t0", font=('Avenir LT Std 35 Light
 current_command = StringVar()
 current_command.set("hi")
 
+# FPS - for debugging
+fps_label = Label(root, text="0 FPS", font=('Avenir LT Std 35 Light', 15))
+
+# Toggle FPS
+def toggle_fps(e):
+    global show_fps
+    if e.char == 'f':
+        show_fps = not show_fps
+
+root.bind("<KeyPress>", toggle_fps)
 
 terminal_entry = Entry(root, textvariable=current_command)
 def commandEntered(p0):
     print("Command: " + terminal_entry.get())
     terminal_entry.delete(0, 'end')
 terminal_entry.bind("<Return>", commandEntered)
+
 # Fans
 f1_slider = Scale(root, from_=100, to=0, label="Fan 1", length=450, width=30, font=('Avenir LT Std 35 Light', 10))
 f2_slider = Scale(root, from_=100, to=0, label="Fan 2", length=450, width=30, font=('Avenir LT Std 35 Light', 10))
 
+fps_label.place(x=10, y=100)
 pid_button.place(x=10, y=150)
 kp_label.place(x=10, y=200)
 ki_label.place(x=10, y=250)
@@ -94,10 +107,15 @@ line, = ax1.plot(xAxis, data)
 toggle_pid()
 
 def animate(i):
-    global current_loop_timestamp, last_loop_timestamp
-    last_loop_timestamp=current_loop_timestamp
-    current_loop_timestamp=time.time()
-    #print(1/(current_loop_timestamp-last_loop_timestamp))
+    global current_loop_timestamp, last_loop_timestamp, show_fps
+    if show_fps:
+        last_loop_timestamp=current_loop_timestamp
+        current_loop_timestamp=time.time()
+        fps = 1/(current_loop_timestamp-last_loop_timestamp)
+        fps_str = "FPS: " + str(fps)[0:4]
+        fps_label.config(text=fps_str)
+    else:
+        fps_label.config(text='')
     add_random_data()
     ax1.clear()
     ax1.plot(xAxis, data)
