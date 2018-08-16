fbeca98c13a8c4df3eb149440dbc9130a7a072c6
Branson Camp <bcamp0@icloud.com>
2018-08-16 14:40:25 -0600

    Added docs, cleaned up code

--- src/gui-beta.py
+++ src/gui-beta.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import *
+from tkinter import Label, Button, StringVar, Entry, Scale
 from tkinter.ttk import Button
 import matplotlib
 matplotlib.use('TkAgg')
@@ -10,18 +10,22 @@ import matplotlib.animation as animation
 import math
 import random
 import sys
-import time # just for debugging purposes, delete later
-from PIL import ImageTk, Image
+import time
 
-current_loop_timestamp=1 #stores the timestamp of the current animation loop execution
-last_loop_timestamp=0 #stores the timestamp of the previous animation loop execution
+current_loop_timestamp=1 # stores the timestamp of the current animation loop execution
+last_loop_timestamp=0 # stores the timestamp of the previous animation loop execution
 
-MAX_LENGTH = 60
-data = []
-xAxis = []
+MAX_LENGTH = 60 # How many data values to keep before dumping
+data = [] # Stores the sensor data to be graphed 
+xAxis = [] # The x-axis grid (1...MAX_LENGTH)
 
-pid_active = True
-show_fps = False
+pid_active = True # Whether the PID Loop is active
+show_fps = False # Whether the FPS is shown
+
+# Initialize the data
+for i in range(MAX_LENGTH):
+        xAxis.append(i)
+        data.append(0)
 
 def toggle_pid():
     global pid_active
@@ -31,65 +35,57 @@ def toggle_pid():
     else:
         pid_button.config(text='PID DISABLED')
 
-def initialize_data():
-    for i in range(MAX_LENGTH):
-        xAxis.append(i)
-        data.append(0)
-
-style.use('fivethirtyeight')
-
-fig = plt.figure()
-
-root = tk.Toplevel()
-root.geometry('1920x1080+0+0')
-root.title('Amorphics')
-
-# Back Splash
-
-backsplash_img = ImageTk.PhotoImage(Image.open('src/bigblob.png'))
-
-backsplash_canvas = Canvas(root, width= 1920, height=1080)
-
-backsplash_canvas.create_image(200, 100, anchor=NW, image=backsplash_img) 
-
-backsplash_canvas.place(x=0, y=0)
-
-canvas = FigureCanvasTkAgg(fig, master=root)
-canvas.get_tk_widget().place(x=800, y=0)
-
-# Tkinter Widgets
-title_label = Label(root, text='AMORPHICS', font=('Aller Display', 70))
-title_label.place(x=10, y=10)
-motor_label = Label(root, text='Motor Position: \t0', font=('Avenir LT Std 35 Light', 30))
-kp_label = Label(root, text='P: \t\t0', font=('Avenir LT Std 35 Light', 30))
-ki_label = Label(root, text='I: \t\t0', font=('Avenir LT Std 35 Light', 30))
-kd_label = Label(root, text='D: \t\t0', font=('Avenir LT Std 35 Light', 30))
-pid_button = Button(root, text='PID DISABLED', width=13, command=toggle_pid)
-diameter_label = Label(root, text='Diameter: \t0', font=('Avenir LT Std 35 Light', 30))
-current_command = StringVar()
-current_command.set('hi')
-
-# FPS - for debugging
-fps_label = Label(root, text='0 FPS', font=('Avenir LT Std 35 Light', 15))
-
 # Toggle FPS
 def toggle_fps(e):
     global show_fps
     if e.char == 'f':
         show_fps = not show_fps
 
-root.bind('<KeyPress>', toggle_fps)
+style.use('fivethirtyeight')
 
+# ---------------- WIDGETS ----------------
+# Root Widget
+fig = plt.figure()
+root = tk.Toplevel()
+root.geometry('1920x1080+0+0')
+root.title('Amorphics')
+root.protocol('WM_DELETE_WINDOW', exit)
+title_label = Label(root, text='AMORPHICS', font=('Aller Display', 70))
+
+# PID Widgets
+kp_label = Label(root, text='P: \t\t0', font=('Avenir LT Std 35 Light', 30))
+ki_label = Label(root, text='I: \t\t0', font=('Avenir LT Std 35 Light', 30))
+kd_label = Label(root, text='D: \t\t0', font=('Avenir LT Std 35 Light', 30))
+pid_button = Button(root, text='PID DISABLED', width=13, command=toggle_pid)
+
+# Graph Widgets
+diameter_label = Label(root, text='Diameter: \t0', font=('Avenir LT Std 35 Light', 30))
+canvas = FigureCanvasTkAgg(fig, master=root)
+canvasWidget = canvas.get_tk_widget()
+
+# Terminal Widgets
+current_command = StringVar()
+current_command.set('hi')
 terminal_entry = Entry(root, textvariable=current_command)
 def commandEntered(p0):
     print('Command: ' + terminal_entry.get())
     terminal_entry.delete(0, 'end')
 terminal_entry.bind('<Return>', commandEntered)
 
-# Fans
+# Fan Widgets
 f1_slider = Scale(root, from_=100, to=0, label='Fan 1', length=450, width=30, font=('Avenir LT Std 35 Light', 10))
 f2_slider = Scale(root, from_=100, to=0, label='Fan 2', length=450, width=30, font=('Avenir LT Std 35 Light', 10))
 
+# Motor Widget
+motor_label = Label(root, text='Motor Position: \t0', font=('Avenir LT Std 35 Light', 30))
+
+# FPS Widget - for debugging
+fps_label = Label(root, text='0 FPS', font=('Avenir LT Std 35 Light', 15))
+root.bind('<KeyPress>', toggle_fps)
+
+# --------- Drawing to the Screen ---------
+canvasWidget.place(x=800, y=0)
+title_label.place(x=10, y=10)
 fps_label.place(x=10, y=100)
 pid_button.place(x=10, y=150)
 kp_label.place(x=10, y=200)
@@ -123,11 +119,6 @@ def animate(i):
     diameter_label.config(text=dia_str)
     return ax1.lines[0],
 
-def pop_data():
-    for i in range(len(data)-1):
-        data[i] = data[i+1]
-    del data[-1]
-
 def add_random_data():
     num = random.randrange(1023)
     add_data(num)
@@ -135,15 +126,7 @@ def add_random_data():
 def add_data(num):
     data.append(num)
     if len(data) > MAX_LENGTH:
-        pop_data()
+        del data[0]
 
-
-def on_closing():
-    root.quit()
-    root.destroy()
-    sys.exit(0)
-
-root.protocol('WM_DELETE_WINDOW', on_closing)
-
-ani = animation.FuncAnimation(fig, animate, init_func = initialize_data(), interval=1, blit=True)
+ani = animation.FuncAnimation(fig, animate, interval=300, blit=True)
 root.mainloop()
